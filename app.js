af570fba8539845c248130c4b56e589dcdf956e8
Amr Elzahar <amr92621@gmail.com>
2023-03-02 07:17:55 +0200

    Removing internal ip

--- app.js
+++ app.js
@@ -58,6 +58,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, "192.168.1.5", () =>
-  console.log("Server is listening on port 3000...")
-);
+server.listen(3000, () => console.log("Server is listening on port 3000..."));
